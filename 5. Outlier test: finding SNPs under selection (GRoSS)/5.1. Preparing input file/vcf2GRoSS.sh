3887df587e623c0f2893c83665de3237f4a529c2
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-06-24 16:04:53 +0200

    Update vcf2GRoSS.sh

--- 5. Outlier test: finding SNPs under selection (GRoSS)/5.1. Preparing input file/vcf2GRoSS.sh
+++ 5. Outlier test: finding SNPs under selection (GRoSS)/5.1. Preparing input file/vcf2GRoSS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-output="Master-s-Thesis-/Outlier_test/Run_GRoSS"
+out_file="Master-Thesis-MBC/5. Outlier test: finding SNPs under selection (GRoSS)/5.2. Running GRoSS/Inputs/"
 
-./glactools/glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai hgdp_tgp_sgdp_chr12_p.dated_287.vcf | ./glactools/glactools meld -f panel.txt - | ./glactools/glactools acf2gross --noroot - | gzip > $output/"human_chr12.gross.gz"
+./glactools/glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai hgdp_tgp_sgdp_chr12_p.dated_287.vcf | ./glactools/glactools meld -f panel.txt - | ./glactools/glactools acf2gross --noroot - | gzip > $out_file/"human_chr12.gross.gz"
