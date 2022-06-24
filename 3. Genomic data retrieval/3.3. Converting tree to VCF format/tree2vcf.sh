df67da7ab6716caca2c837e62e20ec2615784d6c
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-06-24 13:00:59 +0200

    Update tree2vcf.sh

--- 3. Genomic data retrieval/3.3. Converting tree to VCF format/tree2vcf.sh
+++ 3. Genomic data retrieval/3.3. Converting tree to VCF format/tree2vcf.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# hgdp_tgp_sgdp_chr12_p.dated.trees downloaded from Zenodo
+# file too large to upload it to this repository
 python3 -m tskit vcf hgdp_tgp_sgdp_chr12_p.dated.trees > hgdp_tgp_sgdp_chr12_p.dated.vcf
