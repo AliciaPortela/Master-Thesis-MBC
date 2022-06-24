45e1bfd2701cd0a95306ad359f5e8078ea6d878a
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-06-24 16:36:58 +0200

    Update filter_ind.sh

--- 3. Genomic data retrieval/3.4. Filtering VCF file by individuals/filter_ind.sh
+++ 3. Genomic data retrieval/3.4. Filtering VCF file by individuals/filter_ind.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# filtering the VCF file by 287 individuals (merged individuals)
 vcftools --vcf hgdp_tgp_sgdp_chr12_p.dated.vcf --keep samples.txt --recode --stdout | bgzip -c > hgdp_tgp_sgdp_chr12_p.dated_287.vcf.gz
-# This is our final vcf
+
+# hgdp_tgp_sgdp_chr12_p.dated_287.vcf.gz is the final VCF file 
