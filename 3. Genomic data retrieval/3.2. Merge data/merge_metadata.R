0f18e84cf4afe2cde59df40c57bbed81cf729e29
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-06-24 16:33:36 +0200

    Update merge_metadata.R

--- 3. Genomic data retrieval/3.2. Merge data/merge_metadata.R
+++ 3. Genomic data retrieval/3.2. Merge data/merge_metadata.R
@@ -1,10 +1,14 @@
 #!/usr/bin/env Rscript
 
+# data of 3754 individuals from Wohns et al., 2022 database
 all <- read.csv("ind_list.csv")
 
 setwd("Master-Thesis-MBC/2. Selection of human populations and genomic samples/Environmental divergence index/selected_samples.csv")
+# data of 330 individuals selected for this study
 samples <- read.csv("selected_samples.csv", sep = "\t")
+# merge both data 
 merged <- merge(all, samples, by.x = "FINAL")
 
 setwd("Master-Thesis-MBC/3. Genomic data retrieval/3.2. Merge data/")
+# write a table with the merged data (287 individuals)
 write.csv(merged, "merged.csv")
